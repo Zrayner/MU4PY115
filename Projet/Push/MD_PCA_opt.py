2c7c64140e5b609bee0ecf2862517cbe88663fa2
Ewen Froge <ewen.froge@gmail.com>
2021-01-04 10:54:16 +0100

    Hyperopt param 2

--- Projet/Push/MD_PCA_opt.py
+++ Projet/Push/MD_PCA_opt.py
@@ -297,7 +297,7 @@ def objective(space_params):
     return {'loss': last_loss, 'status': STATUS_OK }
     
 trials=Trials()
-best = fmin(objective, Space, algo=tpe.suggest, trials=trials, max_evals=100)
+best = fmin(objective, Space, algo=tpe.suggest, trials=trials, max_evals=10)
 print (best)
 print (trials.best_trial)
 
