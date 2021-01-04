f18fdf739641d7b5c73f9cd6724b4a033282d160
Alexzakkk <alexandre.antezak@gmail.com>
2021-01-04 09:35:51 +0100

    Alex

--- Projet/alex/NN_pca_final+MC.py
+++ Projet/alex/NN_pca_final+MC.py
@@ -279,14 +279,12 @@ def get_energy(positions):
     for i_hydrogens in range(n_hydrogens):
         scaler_H_1[i_hydrogens].transform(descriptors[i_hydrogens+n_oxygens,:].reshape(1,-1))
         pca_hydrogens.transform(descriptors[i_hydrogens+n_oxygens,:].reshape(1,-1))
-        for j_dims in range(pca_treshold):
-            scaler_H_2[i_hydrogens].transform(descriptors[i_hydrogens+n_oxygens,j_dims].reshape(-1,1))
+        scaler_H_2[i_hydrogens].transform(descriptors[i_hydrogens+n_oxygens,:pca_treshold].reshape(1,-1))
 
     for i_oxygens in range(n_oxygens):
         scaler_O_1[i_oxygens].transform(descriptors[i_oxygens,:].reshape(1,-1))
         pca_oxygens.transform(descriptors[i_oxygens,:].reshape(1,-1))
-        for j_dims in range(pca_treshold):
-            scaler_O_2[i_oxygens].transform(descriptors[i_oxygens,j_dims].reshape(-1,1))
+        scaler_O_2[i_oxygens].transform(descriptors[i_oxygens,:pca_treshold].reshape(1,-1))
    
     
     descriptors_nn =[]
