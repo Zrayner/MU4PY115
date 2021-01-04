6d2887373fcda7a2fd7d27c6d61fcadeff4d9f9f
Alexzakkk <alexandre.antezak@gmail.com>
2021-01-04 13:58:39 +0100

    erreurs

--- Projet/alex/NN_pca_final+MC.py
+++ Projet/alex/NN_pca_final+MC.py
@@ -277,15 +277,15 @@ def get_energy(positions):
     zundel = Atoms(numbers=[8,8,1,1,1,1,1], positions=positions)
     descriptors = soap.create(zundel,positions=np.arange(n_atoms),n_jobs=4)
     print(np.shape(descriptors))     
-    descriptors[n_oxygens:,:] = scaler_H_1.transform(descriptors[n_oxygens:,:].reshape(1,-1))
+    descriptors[n_oxygens:,:] = scaler_H_1.transform(descriptors[n_oxygens:,:])
     for i_hydrogens in range(n_hydrogens):
-        descriptors[n_oxygens+i_hydrogens,:] = pca_hydrogens.transform(descriptors[n_oxygens+i_hydrogens,:].reshape(1,-1))
-    descriptors[n_oxygens:,:pca_treshold] = scaler_H_2.transform(descriptors[n_oxygens:,:pca_treshold].reshape(1,-1))
+        descriptors[n_oxygens+i_hydrogens,:] = pca_hydrogens.transform(descriptors[n_oxygens+i_hydrogens,:])
+    descriptors[n_oxygens:,:pca_treshold] = scaler_H_2.transform(descriptors[n_oxygens:,:pca_treshold])
     
-    descriptors[:n_oxygens,:] = scaler_O_1.transform(descriptors[:n_oxygens,:].reshape(1,-1))
+    descriptors[:n_oxygens,:] = scaler_O_1.transform(descriptors[:n_oxygens,:])
     for i_oxygens in range(n_oxygens):
-        descriptors[i_oxygens,:] = pca_oxygens.transform(descriptors[i_oxygens,:].reshape(1,-1))
-    descriptors[:n_oxygens,:pca_treshold] =scaler_O_2.transform(descriptors[:n_oxygens,:pca_treshold].reshape(1,-1))
+        descriptors[i_oxygens,:] = pca_oxygens.transform(descriptors[i_oxygens,:])
+    descriptors[:n_oxygens,:pca_treshold] =scaler_O_2.transform(descriptors[:n_oxygens,:pca_treshold])
    
     
     descriptors_nn =[]
