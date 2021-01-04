386fc619e81444ac01c97f89289c64c4b70be98e
Alexzakkk <alexandre.antezak@gmail.com>
2021-01-04 11:30:09 +0100

    Alex

--- Projet/alex/NN_pca_final+MC.py
+++ Projet/alex/NN_pca_final+MC.py
@@ -279,18 +279,15 @@ def get_energy(positions):
     for i_hydrogens in range(n_hydrogens):
         scaler_H_1[i_hydrogens].transform(descriptors[i_hydrogens+n_oxygens,:].reshape(1,-1))
         pca_hydrogens.transform(descriptors[i_hydrogens+n_oxygens,:].reshape(-1,1))
-        print(np.shape(descriptors))
-        scaler_H_2[i_hydrogens].transform(descriptors[i_hydrogens+n_oxygens,:pca_treshold].reshape(1,-1))
 
     for i_oxygens in range(n_oxygens):
         scaler_O_1[i_oxygens].transform(descriptors[i_oxygens,:].reshape(1,-1))
         pca_oxygens.transform(descriptors[i_oxygens,:].reshape(-1,1))
-        scaler_O_2[i_oxygens].transform(descriptors[i_oxygens,:pca_treshold].reshape(1,-1))
    
     
     descriptors_nn =[]
     for i_atom in range(n_atoms):
-        descriptors_nn.append(descriptors[i_atom,:])
+        descriptors_nn.append(descriptors[i_atom,:pca_treshold])
 
     return energies_scaler.inverse_transform(Zundel_NN.predict(descriptors_nn))
     
