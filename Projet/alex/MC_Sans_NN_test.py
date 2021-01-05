973b02852c20a77f214c0e984c71a40a3b3696f1
Alexzakkk <alexandre.antezak@gmail.com>
2021-01-05 10:38:49 +0100

    erreur

--- Projet/alex/MC_Sans_NN_test.py
+++ Projet/alex/MC_Sans_NN_test.py
@@ -174,7 +174,7 @@ dist = np.empty([n_configs-1,3])
 for i_configs in range(n_configs-1):
     for j_pos in range(3):
         dist[i_configs,j_pos] = np.absolute(all_positions[i_configs,2,j_pos]-all_positions[i_configs+1,2,j_pos])
-delta = np.mean(np.mean(dist,axis=0)) * 0.5
+delta = np.mean(np.mean(dist,axis=0)) * 2
 print("delta=",delta)
 
 
