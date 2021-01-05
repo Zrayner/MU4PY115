1447d7bb1c1a17d018300c71866d8cb1ff900df4
Ewen Froge <ewen.froge@gmail.com>
2021-01-05 02:51:09 +0100

    correction

--- Projet/alex/MC_Sans_NN.py
+++ Projet/alex/MC_Sans_NN.py
@@ -29,6 +29,8 @@ from dscribe.descriptors import SOAP
 from ase.build import molecule
 from ase import Atoms
 from keras.models import load_model
+
+
 Zundel_NN = load_model('Fitted_Zundel_NN.h5')
 
 datapath='../../../'
@@ -60,16 +62,113 @@ soap = SOAP(
 
 n_configs = np.shape(positions)[0]
 n_features = soap.get_number_of_features()
+n_dims = n_features
 n_elements = 2
 n_oxygens = 2
 n_hydrogens = 5
 n_atoms = n_hydrogens + n_oxygens
 
+
+#zundel molecule creation
+zundels = np.empty(n_configs,dtype=object )
+for i_configs in range(n_configs):
+      zundels[i_configs] = Atoms(numbers=[8,8,1,1,1,1,1], positions=positions[i_configs])
+
+
+# computing descriptors for each positions
+descriptors=np.empty([n_configs,n_atoms,n_features])
+for i_configs in range(n_configs):
+    descriptors[i_configs,:,:] = soap.create(zundels[i_configs],positions=np.arange(n_atoms),n_jobs=4)
+print('soap ok')
+
+#scaling inputs and outputs
+energies_scaler = StandardScaler().fit(energies.reshape((-1,1))) 
+scaled_energies = energies_scaler.transform(energies.reshape((-1,1)))
+
+
+
+n_features_oxygens = n_configs*n_oxygens
+n_features_hydrogens = n_configs*n_hydrogens
+
+
+scaled_descriptors = np.empty([n_features_hydrogens+n_features_oxygens,n_dims])
+
+
 scaler_O_1 = StandardScaler()
 scaler_H_1 = StandardScaler()
+scaled_descriptors[n_features_oxygens:,:] = scaler_H_1.fit_transform(descriptors.reshape(n_features_hydrogens+n_features_oxygens,n_dims)[n_features_oxygens:,:])
+scaled_descriptors[:n_features_oxygens,:] = scaler_O_1.fit_transform(descriptors.reshape(n_features_hydrogens+n_features_oxygens,n_dims)[:n_features_oxygens,:])
+
+
+
+#PCA
+
+
+var_ratio_pca_oxygens = np.empty(n_features_oxygens)
+var_ratio_pca_hydrogens = np.empty(n_features_hydrogens)   
+
+pca_oxygens = PCA(n_dims)
+pca_hydrogens = PCA(n_dims)
+pca_oxygens.fit(scaled_descriptors[:n_features_oxygens,:])
+pca_hydrogens.fit(scaled_descriptors[n_features_oxygens:,:])
+var_ratio_pca_hydrogens = pca_hydrogens.explained_variance_ratio_
+var_ratio_pca_oxygens = pca_oxygens.explained_variance_ratio_
+
+var_ratio_oxygens = 0
+var_ratio_hydrogens = 0
+pca_treshold_hydrogens = 0
+pca_treshold_oxygens = 0
+
+while var_ratio_hydrogens<0.999999:
+    var_ratio_hydrogens +=  var_ratio_pca_hydrogens[pca_treshold_hydrogens]
+    pca_treshold_hydrogens += 1
+    
+while var_ratio_oxygens<0.999999:
+    var_ratio_oxygens += var_ratio_pca_oxygens[pca_treshold_oxygens]
+    pca_treshold_oxygens += 1
+        
+
+pca_treshold = max(pca_treshold_hydrogens,pca_treshold_oxygens)
+print("dimension desc post pca=", pca_treshold, "\n"
+      "dimennsion desc pre pca=",n_dims)
+
+scaled_pca_descriptors = np.empty([n_configs,n_atoms,n_dims])
+for i_hydrogens in range(n_hydrogens):
+    scaled_pca_descriptors[:,i_hydrogens+n_oxygens,:] = pca_hydrogens.transform(scaled_descriptors.reshape(n_configs,n_atoms,n_dims)[:,i_hydrogens+n_oxygens,:])
+for i_oxygens in range(n_oxygens):
+    scaled_pca_descriptors[:,i_oxygens,:] = pca_oxygens.transform(scaled_descriptors.reshape(n_configs,n_atoms,n_dims)[:,i_oxygens,:])
+    
+#scaling post pca
+
+
 
 scaler_O_2 = MinMaxScaler()
 scaler_H_2 = MinMaxScaler()
+
+scaled_pca_descriptors.reshape(n_features_hydrogens+n_features_oxygens,n_dims)[n_features_oxygens:,:pca_treshold] = scaler_H_2.fit_transform(scaled_descriptors.reshape(n_features_hydrogens+n_features_oxygens,n_dims)[n_features_oxygens:,:pca_treshold])
+scaled_pca_descriptors.reshape(n_features_hydrogens+n_features_oxygens,n_dims)[:n_features_oxygens,:pca_treshold] = scaler_O_2.fit_transform(scaled_descriptors.reshape(n_features_hydrogens+n_features_oxygens,n_dims)[:n_features_oxygens,:pca_treshold])
+
+#swaping axes for NN purpose
+descriptors_swap = np.swapaxes(scaled_pca_descriptors.reshape(n_configs,n_atoms,n_dims)[:,:,:pca_treshold],0,1)
+
+
+#setting the train and test and validation set
+descriptors_train = descriptors_swap[:,:85000,:]
+descriptors_val = descriptors_swap[:,85000:95000,:]
+descriptors_test = descriptors_swap[:,95000:,:]
+energies_train = scaled_energies[:85000]
+energies_val = scaled_energies[85000:95000]
+energies_test = scaled_energies[95000:]
+
+
+#creating a list of array to fit in the NN
+descriptors_train_nn = []
+descriptors_test_nn = []
+descriptors_val_nn = []
+for i_atom in range(n_atoms):
+    descriptors_train_nn.append(descriptors_train[i_atom,:,:])
+    descriptors_test_nn.append(descriptors_test[i_atom,:,:])
+    descriptors_val_nn.append(descriptors_val[i_atom,:,:])
 
 def get_energy(positions):
 
