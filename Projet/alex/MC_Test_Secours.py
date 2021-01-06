41146ae9a8d6ad3d1c843b13144a1062a6bbe654
Ewen Froge <ewen.froge@gmail.com>
2021-01-06 14:28:13 +0100

    Opt Delta

--- Projet/alex/MC_Test_Secours.py
+++ Projet/alex/MC_Test_Secours.py
@@ -179,8 +179,8 @@ delta = (max(np.max(np.max(dist,axis=0),axis=1))- min(np.min(np.min(dist,axis=0)
 print("delta=",delta)
 
 
-mc_time = 100
-mc_iterations = 40
+mc_time = 50
+mc_iterations = 10
 acceptation = []
 hartree = 1.602176*27.211297e-19
 
