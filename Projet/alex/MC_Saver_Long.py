24408c87c91067e1a224e125e4514d7cddca3840
Alexzakkk <alexandre.antezak@gmail.com>
2021-01-06 22:40:53 +0100

    MC termine

--- Projet/alex/MC_Saver_Long.py
+++ Projet/alex/MC_Saver_Long.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jan  6 14:45:23 2021
-
-@author: ewenf
-"""
-
 from __future__ import print_function
 import tensorflow as tf
 import keras,sklearn
@@ -20,58 +13,119 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 seed=0
 np.random.seed(seed) # fix random seed
 
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.models import load_model
+
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler, MaxAbsScaler, MinMaxScaler
 
 from dscribe.descriptors import SOAP
 from ase.build import molecule
 from ase import Atoms
-from ase.io import write
-from keras.models import load_model
-
-
-Zundel_NN = load_model('Fitted_Zundel_NN.h5')
 
 datapath='../../../'
 #positions and corresponding energies of a zundel molecule importation
 all_positions = pickle.load(open(os.path.join(datapath,'zundel_100K_pos'),'rb'))
 all_energies = pickle.load(open(os.path.join(datapath,'zundel_100K_energy'),'rb'))[1:]
 
-positions = all_positions[::5]
-energies = all_energies[::5]
+
 
 #parameters settings
-species = ["H","O"]
-sigma_SOAP = 1
-periodic = False
-nmax = 4
-lmax = 5
-rcut = 11.0
 
-#soap settings
-soap = SOAP(
-    species=species,
-    sigma=sigma_SOAP,
-    periodic=False,
-    rcut=rcut,
-    nmax=nmax,
-    lmax=lmax,
-    sparse=False,
+molecule_params={
+        'n_elements': 2,
+        'n_oxygens': 2,
+        'n_hydrogens': 5,
+        'molecules_order':[8,8,1,1,1,1,1]
+ 
+       
+    
+    }
+
+
+dscribe_params={
+        'nmax': 4,
+        'lmax': 5,
+        'rcut': 11.0,
+        'sigma_SOAP': 1.0,
+        'periodic': False,
+        'species': ["H","O"],
+        'sparse':False,
+ 
+       
+    
+    }
+
+model_params={
+        'layers_units': 30,
+        'layers_number': 2,
+        'kernel_initializer': None,
+ 
+       
+    
+    }
+
+
+data_params={
+        'slicing': 5,
+        'train_ratio':0.85,
+        'val_ratio':0.1,
+  
+ 
+       
+    
+    }
+
+PCA_params={
+        'Scaler_Pre_PCA': StandardScaler(),
+        'Scaler_Post_PCA':StandardScaler(),
+        'pca_pourcentage':0.999999,
+  
+ 
+       
+    
+    }
+
+
+fit_params={
+        'epochs': 500,
+        'batch_size':30,
+        'verbose':1,
+  
+ 
+       
+    
+    }
+
+
+#data_slicing
+positions = all_positions[::data_params['slicing']]
+energies = all_energies[::data_params['slicing']]
+
+
+#soap descriptors
+soap = SOAP(nmax=dscribe_params['nmax'],
+            lmax=dscribe_params['lmax'],
+            rcut=dscribe_params['rcut'],
+            sigma=dscribe_params['sigma_SOAP'],
+            periodic=dscribe_params['periodic'],
+            species=dscribe_params['species'],
+            sparse=dscribe_params['sparse'],
+            
 )
 
 n_configs = np.shape(positions)[0]
 n_features = soap.get_number_of_features()
 n_dims = n_features
-n_elements = 2
-n_oxygens = 2
-n_hydrogens = 5
-n_atoms = n_hydrogens + n_oxygens
+
+n_atoms = molecule_params['n_hydrogens'] + molecule_params['n_oxygens']
 
 
 #zundel molecule creation
 zundels = np.empty(n_configs,dtype=object )
 for i_configs in range(n_configs):
-      zundels[i_configs] = Atoms(numbers=[8,8,1,1,1,1,1], positions=positions[i_configs])
+      zundels[i_configs] = Atoms(numbers=molecule_params['molecules_order'], positions=positions[i_configs])
 
 
 # computing descriptors for each positions
@@ -86,15 +140,15 @@ scaled_energies = energies_scaler.transform(energies.reshape((-1,1)))
 
 
 
-n_features_oxygens = n_configs*n_oxygens
-n_features_hydrogens = n_configs*n_hydrogens
+n_features_oxygens = n_configs*molecule_params['n_oxygens']
+n_features_hydrogens = n_configs*molecule_params['n_hydrogens']
 
 
 scaled_descriptors = np.empty([n_features_hydrogens+n_features_oxygens,n_dims])
 
 
-scaler_O_1 = StandardScaler()
-scaler_H_1 = StandardScaler()
+scaler_O_1 = PCA_params['Scaler_Pre_PCA']
+scaler_H_1 = PCA_params['Scaler_Pre_PCA']
 scaled_descriptors[n_features_oxygens:,:] = scaler_H_1.fit_transform(descriptors.reshape(n_features_hydrogens+n_features_oxygens,n_dims)[n_features_oxygens:,:])
 scaled_descriptors[:n_features_oxygens,:] = scaler_O_1.fit_transform(descriptors.reshape(n_features_hydrogens+n_features_oxygens,n_dims)[:n_features_oxygens,:])
 
@@ -116,11 +170,11 @@ var_ratio_hydrogens = 0
 pca_treshold_hydrogens = 0
 pca_treshold_oxygens = 0
 
-while var_ratio_hydrogens<0.999999:
+while var_ratio_hydrogens<PCA_params['pca_pourcentage']:
     var_ratio_hydrogens +=  var_ratio_pca_hydrogens[pca_treshold_hydrogens]
     pca_treshold_hydrogens += 1
     
-while var_ratio_oxygens<0.999999:
+while var_ratio_oxygens<PCA_params['pca_pourcentage']:
     var_ratio_oxygens += var_ratio_pca_oxygens[pca_treshold_oxygens]
     pca_treshold_oxygens += 1
         
@@ -130,19 +184,46 @@ print("dimension desc post pca=", pca_treshold, "\n"
       "dimennsion desc pre pca=",n_dims)
 
 scaled_pca_descriptors = np.empty([n_configs,n_atoms,n_dims])
-for i_hydrogens in range(n_hydrogens):
-    scaled_pca_descriptors[:,i_hydrogens+n_oxygens,:] = pca_hydrogens.transform(scaled_descriptors.reshape(n_configs,n_atoms,n_dims)[:,i_hydrogens+n_oxygens,:])
-for i_oxygens in range(n_oxygens):
+for i_hydrogens in range(molecule_params['n_hydrogens'] ):
+    scaled_pca_descriptors[:,i_hydrogens+molecule_params['n_oxygens'],:] = pca_hydrogens.transform(scaled_descriptors.reshape(n_configs,n_atoms,n_dims)[:,i_hydrogens+molecule_params['n_oxygens'],:])
+for i_oxygens in range(molecule_params['n_oxygens']):
     scaled_pca_descriptors[:,i_oxygens,:] = pca_oxygens.transform(scaled_descriptors.reshape(n_configs,n_atoms,n_dims)[:,i_oxygens,:])
     
 #scaling post pca
-scaler_O_2 = StandardScaler()
-scaler_H_2 = StandardScaler()
+scaler_O_2 = PCA_params['Scaler_Post_PCA']
+scaler_H_2 = PCA_params['Scaler_Post_PCA']
+
 
 scaled_pca_descriptors.reshape(n_features_hydrogens+n_features_oxygens,n_dims)[n_features_oxygens:,:pca_treshold] = scaler_H_2.fit_transform(scaled_descriptors.reshape(n_features_hydrogens+n_features_oxygens,n_dims)[n_features_oxygens:,:pca_treshold])
 scaled_pca_descriptors.reshape(n_features_hydrogens+n_features_oxygens,n_dims)[:n_features_oxygens,:pca_treshold] = scaler_O_2.fit_transform(scaled_descriptors.reshape(n_features_hydrogens+n_features_oxygens,n_dims)[:n_features_oxygens,:pca_treshold])
 
 
+#swaping axes for NN purpose
+descriptors_swap = np.swapaxes(scaled_pca_descriptors.reshape(n_configs,n_atoms,n_dims)[:,:,:pca_treshold],0,1)
+
+train_limit = int(data_params['train_ratio']*n_configs)
+val_limit = int((data_params['train_ratio']+data_params['val_ratio'])*n_configs)
+
+
+#setting the train and test and validation set
+descriptors_train = descriptors_swap[:,:train_limit,:]
+descriptors_val = descriptors_swap[:,train_limit:val_limit,:]
+descriptors_test = descriptors_swap[:,val_limit:,:]
+energies_train = scaled_energies[:train_limit]
+energies_val = scaled_energies[train_limit:val_limit]
+energies_test = scaled_energies[val_limit:]
+
+
+#creating a list of array to fit in the NN
+descriptors_train_nn = []
+descriptors_test_nn = []
+descriptors_val_nn = []
+for i_atom in range(n_atoms):
+    descriptors_train_nn.append(descriptors_train[i_atom,:,:])
+    descriptors_test_nn.append(descriptors_test[i_atom,:,:])
+    descriptors_val_nn.append(descriptors_val[i_atom,:,:])
+
+#energy function: getting energy with the NN predictions over positions
 def get_energy(positions):
 
     zundel = Atoms(numbers=[8,8,1,1,1,1,1], positions=positions)
@@ -165,32 +246,20 @@ def get_energy(positions):
     
     return energies_scaler.inverse_transform(Zundel_NN.predict(descriptors_nn))[0][0]
 
-
+#Temperature and Boltzmann constant
 T = 100
 k = 1.380649e-23
 beta = 1/(T*k)
 
 
-"""dist = np.empty([n_configs-1,n_atoms,3])
-for i_configs in range(n_configs-1):
-    for i_atom in range(n_atoms):
-        for j_pos in range(3):
-            dist[i_configs,i_atom,j_pos] = np.absolute(all_positions[i_configs,i_atom,j_pos]-all_positions[i_configs+1,i_atom,j_pos])
-delta = (max(np.max(np.max(dist,axis=0),axis=1))- min(np.min(np.min(dist,axis=0),axis=1))) * 0.1 #facteur tq taux acceptation = 0.4
-print("delta=",delta)"""
+mc_time = 100000 #iterations for MC
 
+acceptation = [] 
+hartree = 1.602176*27.211297e-19 #covert hartree to Joules
 
-mc_time = 100000
+delta=0.01 #lenght of the box where atoms are moving
 
-acceptation = []
-hartree = 1.602176*27.211297e-19
-
-delta=0.01
-save_ratio=0.1
-
-
-
-
+#save MC positions over time
 def save(i_time,acceptation,guess_positions_overtime,guess_energy_overtime):
     print('saving')
     print("taux d'acceptation=",np.mean(acceptation))  
@@ -209,18 +278,18 @@ def save(i_time,acceptation,guess_positions_overtime,guess_energy_overtime):
 
 
 
-
+#creating MC positions and energies array
 guess_energy_overtime = np.empty(mc_time)
 guess_positions_overtime = np.empty([mc_time,n_atoms,3])
 guess_positions_overtime[0] = all_positions[0,:,:]
 
 
-i_time=1
-while i_time<mc_time:
-    
-    print(i_time/mc_time*100,'%')
+i_time=1 #initializing MC iteration
 
-    
+#MC simulation
+while i_time<mc_time:
+
+    print(i_time/mc_time*100,'%')
 
     increment_aleatoire = np.random.random((n_atoms,3))*2*delta - delta 
     try_position = guess_positions_overtime[i_time-1,:,:] + increment_aleatoire 
@@ -230,22 +299,22 @@ while i_time<mc_time:
     diff_E = (try_energy - guess_energy_overtime[i_time-1]) * hartree  #1 hartree = 27,211396641308eV
     if diff_E < 0 : 
         guess_positions_overtime[i_time] = try_position
-        guess_energy_overtime[i_time]=try_energy
+        guess_energy_overtime[i_time] = try_energy
         i_time = i_time + 1
         acceptation.append(1)
     elif np.exp(-beta * diff_E) >= np.random.random():
         guess_positions_overtime[i_time] = try_position
-        guess_energy_overtime[i_time]=try_energy
+        guess_energy_overtime[i_time] = try_energy
         i_time = i_time + 1
         acceptation.append(1)
     else:
         acceptation.append(0)
         guess_positions_overtime[i_time] = guess_positions_overtime[i_time-1] 
-        guess_energy_overtime[i_time]=guess_energy_overtime[i_time-1]
+        guess_energy_overtime[i_time] = guess_energy_overtime[i_time-1]
         i_time+=1
 
 
-    
+#save the data
 save(mc_time-1,acceptation,guess_positions_overtime,guess_energy_overtime)
 
 
