91145e5b2ec8789d4656b3fa40fefa921b31b263
Ewen Froge <ewen.froge@gmail.com>
2021-01-06 13:56:45 +0100

    Opt Delta

--- Projet/alex/MC_Test_Secours.py
+++ Projet/alex/MC_Test_Secours.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  6 13:49:58 2021
+
+@author: ewenf
+"""
+
 from __future__ import print_function
 import tensorflow as tf
 import keras,sklearn
@@ -13,15 +20,17 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 seed=0
 np.random.seed(seed) # fix random seed
 
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten
-
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler, MaxAbsScaler, MinMaxScaler
 
 from dscribe.descriptors import SOAP
 from ase.build import molecule
 from ase import Atoms
+from ase.io import write
+from keras.models import load_model
+
+
+Zundel_NN = load_model('Fitted_Zundel_NN.h5')
 
 datapath='../../../'
 #positions and corresponding energies of a zundel molecule importation
@@ -33,35 +42,21 @@ energies = all_energies[::5]
 
 #parameters settings
 species = ["H","O"]
-
-dscribe_params={
-        'nmax': 4,
-        'lmax': 5,
-        'rcut': 11.0,
-        'sigma_SOAP': 1.0,
-        'periodic': False,
-        'species': ["H","O"],
-        'sparse'=False,
- 
-       
-    
-    }
-
-model_params={
-        'layers_units': 30,
-        'layers_number': 2,
-        'kernel_initializer': None,
- 
-       
-    
-    }
-
-
-
-
+sigma_SOAP = 1
+periodic = False
+nmax = 4
+lmax = 5
+rcut = 11.0
 
 #soap settings
-soap = SOAP(dscribe_params
+soap = SOAP(
+    species=species,
+    sigma=sigma_SOAP,
+    periodic=False,
+    rcut=rcut,
+    nmax=nmax,
+    lmax=lmax,
+    sparse=False,
 )
 
 n_configs = np.shape(positions)[0]
@@ -106,8 +101,6 @@ scaled_descriptors[:n_features_oxygens,:] = scaler_O_1.fit_transform(descriptors
 
 
 #PCA
-
-
 var_ratio_pca_oxygens = np.empty(n_features_oxygens)
 var_ratio_pca_hydrogens = np.empty(n_features_hydrogens)   
 
@@ -143,130 +136,91 @@ for i_oxygens in range(n_oxygens):
     scaled_pca_descriptors[:,i_oxygens,:] = pca_oxygens.transform(scaled_descriptors.reshape(n_configs,n_atoms,n_dims)[:,i_oxygens,:])
     
 #scaling post pca
-
-
-
 scaler_O_2 = StandardScaler()
 scaler_H_2 = StandardScaler()
 
 scaled_pca_descriptors.reshape(n_features_hydrogens+n_features_oxygens,n_dims)[n_features_oxygens:,:pca_treshold] = scaler_H_2.fit_transform(scaled_descriptors.reshape(n_features_hydrogens+n_features_oxygens,n_dims)[n_features_oxygens:,:pca_treshold])
 scaled_pca_descriptors.reshape(n_features_hydrogens+n_features_oxygens,n_dims)[:n_features_oxygens,:pca_treshold] = scaler_O_2.fit_transform(scaled_descriptors.reshape(n_features_hydrogens+n_features_oxygens,n_dims)[:n_features_oxygens,:pca_treshold])
 
-#swaping axes for NN purpose
-descriptors_swap = np.swapaxes(scaled_pca_descriptors.reshape(n_configs,n_atoms,n_dims)[:,:,:pca_treshold],0,1)
 
+def get_energy(positions):
 
-#setting the train and test and validation set
-descriptors_train = descriptors_swap[:,:85000*2,:]
-descriptors_val = descriptors_swap[:,85000*2:95000*2,:]
-descriptors_test = descriptors_swap[:,95000*2:,:]
-energies_train = scaled_energies[:85000*2]
-energies_val = scaled_energies[85000*2:95000*2]
-energies_test = scaled_energies[95000*2:]
-
-
-#creating a list of array to fit in the NN
-descriptors_train_nn = []
-descriptors_test_nn = []
-descriptors_val_nn = []
-for i_atom in range(n_atoms):
-    descriptors_train_nn.append(descriptors_train[i_atom,:,:])
-    descriptors_test_nn.append(descriptors_test[i_atom,:,:])
-    descriptors_val_nn.append(descriptors_val[i_atom,:,:])
-
-
-
-
-def model(params):
-        
-    model = Sequential()
-    for i in range(params['layers_number']):
-        model.add(Dense(params['layers_units'], activation='tanh',kernel_initializer=params['kernel_initializer']))
-    model.add(Dense(1,))
-  
-        
-    return model
-
-
-
-model0 = model(model_params)
-modelH = model(model_params)
-
-inputs = []
-for i_atoms in range(n_atoms):
-    inputs.append(keras.layers.Input(shape=(pca_treshold,)))
-
-subnets = []
-for i_oxygens in range(n_oxygens):
-    subnets.append(model0(inputs[i_oxygens]))
-for j_hydrogens in range(n_hydrogens):
-    subnets.append(modelH(inputs[i_hydrogens+n_oxygens]))
+    zundel = Atoms(numbers=[8,8,1,1,1,1,1], positions=positions)
+    descriptors = soap.create(zundel,positions=np.arange(n_atoms),n_jobs=4)   
+    descriptors[n_oxygens:,:] = scaler_H_1.transform(descriptors[n_oxygens:,:])
+    for i_hydrogens in range(n_hydrogens):
+        descriptors[n_oxygens+i_hydrogens,:] = pca_hydrogens.transform(descriptors[n_oxygens+i_hydrogens,:].reshape(1,-1))
+    descriptors[n_oxygens:,:pca_treshold] = scaler_H_2.transform(descriptors[n_oxygens:,:pca_treshold])
     
-
-added = keras.layers.Add()(subnets)
-zundel_model = keras.models.Model(inputs, outputs=added)
-
-
-
-def compile_model(model):
+    descriptors[:n_oxygens,:] = scaler_O_1.transform(descriptors[:n_oxygens,:])
+    for i_oxygens in range(n_oxygens):
+        descriptors[i_oxygens,:] = pca_oxygens.transform(descriptors[i_oxygens,:].reshape(1,-1))
+    descriptors[:n_oxygens,:pca_treshold] =scaler_O_2.transform(descriptors[:n_oxygens,:pca_treshold])
+   
+    desc = np.ones([1,pca_treshold])
+    descriptors_nn =[]
+    for i_atom in range(n_atoms):
+        desc[:,:] = descriptors[i_atom,:pca_treshold]
+        descriptors_nn.append(np.int_(desc))
     
-    model.compile(loss=keras.losses.mse,
-                  optimizer=keras.optimizers.Adam())
-    return model
+    return energies_scaler.inverse_transform(Zundel_NN.predict(descriptors_nn))[0][0]
 
 
-
-Zundel_NN = compile_model(zundel_model)
-
-batchsize = 200
-epochs= 10
-
-#callbacks
-lr_reduce = keras.callbacks.ReduceLROnPlateau(
-    monitor='loss', factor=0.5, patience=4, verbose=0,
-    mode='auto', min_delta=0.0001, cooldown=0, min_lr=1e-12
-)
+T = 100
+k = 1.380649e-23
+beta = 1/(T*k)
 
 
-early_stopping = keras.callbacks.EarlyStopping(monitor='loss',min_delta=0.0001,restore_best_weights=True, patience=20)
-
-#training the NN
-history = Zundel_NN.fit(descriptors_train_nn,energies_train,
-                                      batch_size=batchsize,
-                                      epochs=epochs,
-                                      verbose=1,
-                                      callbacks=[early_stopping,lr_reduce],
-                                      validation_data=(descriptors_val_nn,energies_val))
+dist = np.empty([n_configs-1,n_atoms,3])
+for i_configs in range(n_configs-1):
+    for i_atom in range(n_atoms):
+        for j_pos in range(3):
+            dist[i_configs,i_atom,j_pos] = np.absolute(all_positions[i_configs,i_atom,j_pos]-all_positions[i_configs+1,i_atom,j_pos])
+delta = (max(np.max(np.max(dist,axis=0),axis=1))- min(np.min(np.min(dist,axis=0),axis=1))) * 0.4 #facteur tq taux acceptation = 0.4
+print("delta=",delta)
 
 
+mc_time = 800
+mc_iterations = 30
+acceptation = []
+hartree = 1.602176*27.211297e-19
+
+delta=0.385
+
+guess_energy_overtime = np.empty(mc_time)
+guess_positions_overtime = np.empty([mc_time,n_atoms,3])
+guess_positions_overtime[0] = all_positions[0,:,:]
 
 
-"""Zundel_NN.save('Fitted_Zundel_NN.h5')
-
-#descaling energies and outputs
-predicted_energies = Zundel_NN.predict(descriptors_test_nn)
-descaled_energies = energies_scaler.inverse_transform(scaled_energies)
-descaled_predicted_energies = energies_scaler.inverse_transform(predicted_energies)
-energy = np.linspace(0,0.008,200)
-print("max error=",max(np.absolute(descaled_energies[95000*2:]-descaled_predicted_energies)))
-plt.figure(figsize=[10, 5])
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.ylabel('model loss')
-plt.xlabel('epoch')
-plt.grid()
-plt.legend(['train', 'val'], loc='best')
-plt.savefig('loss_over_epoch.jpg')
-plt.clf()
-plt.figure(figsize=[7,7])
-plt.plot(descaled_energies[95000*2:],descaled_predicted_energies,'.',markersize=2)
-plt.plot(energy,energy,markersize=2,color='red')
-plt.xlabel('True Energies (en Hartree)')
-plt.ylabel('Predicted Energies (en Hartree)')
-plt.legend(['Predicted Energy'], loc='best')
-plt.savefig('comparaison.jpg')"""
-
-
-
-
+for delta in [0.0001,0.01,0.5,1,0.385]:
+    print(i_time/mc_time*100,'%')
+    accepted_try_positions = np.empty([mc_iterations,n_atoms,3])
+    accepted_try_energies = np.empty(mc_iterations)
+    n_iterations = 0
+    list_acceptation=[]
+    for i in range(20):
+        acceptation = []
+        while n_iterations < mc_iterations:
+            increment_aleatoire = np.random.random((n_atoms,3))*2*delta - delta 
+            try_position = guess_positions_overtime[i_time-1,:,:] + increment_aleatoire 
+            try_energy = get_energy(try_position)
+    
+        
+            diff_E = (try_energy - guess_energy_overtime[i_time-1]) * hartree  #1 hartree = 27,211396641308eV
+            if diff_E < 0 : 
+                accepted_try_energies[n_iterations] = try_energy
+                accepted_try_positions[n_iterations,:,:] = try_position
+                n_iterations = n_iterations + 1
+                acceptation.append(1)
+            elif np.exp(-beta * diff_E) >= np.random.random():
+                accepted_try_energies[n_iterations] = try_energy
+                accepted_try_positions[n_iterations,:,:] = try_position
+                n_iterations = n_iterations + 1
+                acceptation.append(1)
+            else:
+                acceptation.append(0)
+        list_acceptation.append(np.mean(acceptation))
+    print(np.mean(list_acceptation))
+    
+ 
 
